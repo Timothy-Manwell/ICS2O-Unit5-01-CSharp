15d08604aeceac12676003d15584e3fde83e167b
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-11-15 19:07:16 -0500

    init

--- main.cs
+++ main.cs
@@ -9,21 +9,21 @@ class Program
     public static void Main(string[] args)
     {
         Random random = new Random();
-        int randomNum = random.Next(1,7);
+        int randomNum = random.Next(1, 7);
         int guess;
-      
+
         Console.WriteLine("Welcome to the number guessing game!");
         Console.WriteLine("\nYou will need to guess a number between 1 and 6.");
         Console.WriteLine("\nGuess the number correct and you win!");
         Console.Write("\nGuess a number: ");
-          guess = Convert.ToInt32(Console.ReadLine());
+        guess = Convert.ToInt32(Console.ReadLine());
 
-        if (randomNum == guess) {
-          Console.WriteLine("\nThe number was " + randomNum + " you got it!");
+        if (randomNum == guess){
+        Console.WriteLine("\nThe number was " + randomNum + " you got it!");
         }
 
-        if (randomNum != guess) {
-          Console.WriteLine("\nThe number was " + randomNum + "." + " Good luck next time.");
+        if (randomNum != guess){
+        Console.WriteLine("\nThe number was " + randomNum + "." + " Good luck next time.");
         }
     }
 }
